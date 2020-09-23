45c6bc42099c2fdc57c7e052697bcd6e7cf558e4
zerobit <orourke_kyle@icloud.com>
2020-09-23 16:45:52 -0400

    tutorial8

---
+++ config/keys.js
@@ -0,0 +1,8 @@
+// add to .gitignore
+
+module.exports = {
+    google: {
+        clientID: "626084749774-jajon3gimlq83vjg27t222sqghgo29u3.apps.googleusercontent.com",
+        clientSecret: "wQCrFEIUS91c1iUAMxKl2XgS"
+    }
+}
